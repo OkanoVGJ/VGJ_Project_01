fdb5e34123e1cf7de25fbdf331b614977bcefaac
OkanoVGJ <k.okano.vgj@gmail.com>
2022-08-18 16:39:37 +0900

    テストシーン追加

---
+++ VGJ_Project_2D/Assets/Scripts/MapChip.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MapChip : MonoBehaviour
+{
+    //ENUM
+    public enum MAPCHIP_TYPE
+    {
+        Type1,
+        Type2,
+        Type3,
+        Type4,
+    };
+
+    //変数
+    [SerializeField] public List<Sprite> sprites = new List<Sprite>();
+    [SerializeField] public MAPCHIP_TYPE mapchipType;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        //if((int)mapchipType < sprites.GetRange())
+          spriteRenderer.sprite = sprites[(int)mapchipType];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("aaa");
+    }
+}
---
+++ VGJ_Project_2D/Assets/Scripts/PortableObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortableObject : MonoBehaviour
+{
+    //ENUM
+    public enum OBJECT_TYPE
+    {
+       OBJ_1,
+       OBJ_2,
+       OBJ_3
+    };
+
+    //変数
+    [SerializeField] public List<Sprite> sprites = new List<Sprite>();
+    [SerializeField] public OBJECT_TYPE objType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprites[(int)objType];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
