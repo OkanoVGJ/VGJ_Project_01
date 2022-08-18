d3e316986633a6687d87093217e9ce853e37f287
IshibashiRyuto <rishibashi.52@gmail.com>
2022-08-18 19:48:17 +0900

    シーン管理システム作成中

---
+++ VGJ_Project_2D/Assets/Scripts/MySceneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MySceneManager : MonoBehaviour
+{
+    [SerializeField]
+    private string NowLoadingScene = "NowLoading";
+
+    public void LoadNextScene(string NextSceneName)
+    {
+        LoadProcess(NextSceneName);
+    }
+
+    async void LoadProcess(string NextSceneName)
+    {
+    }
+}
