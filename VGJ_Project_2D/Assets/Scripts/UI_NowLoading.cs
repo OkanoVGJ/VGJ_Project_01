03da4cf6cc57dfb9af3afe10f48e7d49290f3d51
IshibashiRyuto <rishibashi.52@gmail.com>
2022-08-18 18:55:19 +0900

    ロード待機画面を仮実装

---
+++ VGJ_Project_2D/Assets/Scripts/UI_NowLoading.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_NowLoading : MonoBehaviour
+{
+    [SerializeField]
+    private Text text;
+
+    [SerializeField]
+    private float AnimationDuration;
+
+    [SerializeField]
+    private string[] NowLoadingText;
+
+    int ListIndex = 0;
+
+    private float AnimTimer = 0.0f;
+
+    private bool IsLoading = true;
+    
+    void Start()
+    {
+        AnimTimer = 0.0f;
+        text.text = NowLoadingText[ListIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsLoading)
+        {
+            UpdateLoadingText();
+        }
+    }
+
+    void UpdateLoadingText()
+    {
+        AnimTimer += Time.deltaTime;
+
+        if (AnimTimer >= AnimationDuration)
+        {
+            AnimTimer -= AnimationDuration;
+            ListIndex = (ListIndex + 1) % NowLoadingText.Length;
+            text.text = NowLoadingText[ListIndex];
+        }
+    }
+}
