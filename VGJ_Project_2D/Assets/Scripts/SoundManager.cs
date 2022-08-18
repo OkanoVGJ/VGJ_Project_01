96b954959c1906f3dee4a4b0d2eebf5d11e9d61e
IshibashiRyuto <rishibashi.52@gmail.com>
2022-08-18 18:53:14 +0900

    サウンドマネージャおよびサウンドマネージャシーンを作成

---
+++ VGJ_Project_2D/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioSource BGMSource;
+
+    [SerializeField]
+    private AudioSource[] SESource;
+
+    public void PlaySE(AudioClip SEClip)
+    {
+        foreach(var source in SESource)
+        {
+            if(source.isPlaying)
+            {
+                source.clip = SEClip;
+                source.loop = false;
+                source.Play();
+                break;
+            }
+        }
+    }
+
+    public void PlayBGM(AudioClip BGMClip)
+    {
+        BGMSource.Stop();
+        BGMSource.loop = true;
+        BGMSource.clip = BGMClip;
+        BGMSource.Play();
+    }
+}
