cb20d812cb0f3b4922290a6eaa8bf793fa6ed522
RyutoIshibashi <rishibashi.52@gmail.com>
2022-08-19 11:33:40 +0900

    リザルト画面を最低限で作成

---
+++ VGJ_Project_2D/Assets/Scripts/UI/ResultDataViewer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultDataViewer : MonoBehaviour
+{
+
+    [SerializeField]
+    private UiWD_ResultTreasureBox[] tresureBoxWidget;
+    [SerializeField]
+    private ResultData resultData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < tresureBoxWidget.Length; i++)
+        {
+            if (i < resultData.SaveChestNum) {
+                tresureBoxWidget[i].SetTreasureImage(UiWD_ResultTreasureBox.TreasureType.Save);
+            } 
+            else if( i < resultData.AllChestNum)
+            {
+                tresureBoxWidget[i].SetTreasureImage(UiWD_ResultTreasureBox.TreasureType.Lost);
+            }
+            else
+            {
+                tresureBoxWidget[i].SetTreasureImage(UiWD_ResultTreasureBox.TreasureType.None);
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ VGJ_Project_2D/Assets/Scripts/UI/UiWD_ResultTreasureBox.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiWD_ResultTreasureBox : MonoBehaviour
+{
+    public enum TreasureType
+    {
+        None,
+        Lost,
+        Save
+    }
+
+    [SerializeField]
+    private Image LostTreasureImage;
+
+    [SerializeField]
+    private Image SaveTreasureImage;
+
+    [SerializeField]
+    private TreasureType type = TreasureType.Save;
+
+    public void SetTreasureImage(TreasureType treasureType)
+    {
+        switch (treasureType)
+        {
+            case TreasureType.None:
+                LostTreasureImage.enabled = false;
+                SaveTreasureImage.enabled = false;
+                break;
+            case TreasureType.Lost:
+                LostTreasureImage.enabled = true;
+                SaveTreasureImage.enabled = false;
+                break;
+            case TreasureType.Save:
+                LostTreasureImage.enabled = false;
+                SaveTreasureImage.enabled = true;
+                break;
+            default:
+                LostTreasureImage.enabled = false;
+                SaveTreasureImage.enabled = false;
+                break;
+        }
+    }
+}
