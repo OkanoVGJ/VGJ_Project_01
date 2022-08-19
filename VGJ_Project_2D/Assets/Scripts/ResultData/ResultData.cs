04d4a5a1673a07df9d30843d299484ae30b50940
RyutoIshibashi <rishibashi.52@gmail.com>
2022-08-19 09:58:50 +0900

    リザルトデータアセット追加

---
+++ VGJ_Project_2D/Assets/Scripts/ResultData/ResultData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ResultDataAsset", menuName="ScriptableObjects/CreateResultDataAsset")]
+public class ResultData : ScriptableObject
+{
+    public string StageName = "TestScene";
+    public int SaveChestNum = 0;
+    public float ClearTime = 0.0f;
+    public int ClearTurn = 0;
+    public bool IsClear = false;
+}
