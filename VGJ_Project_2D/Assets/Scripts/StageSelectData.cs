e0669b80b75fec2f15c1ce4a2e6c32ba04ab9db1
RyutoIshibashi <rishibashi.52@gmail.com>
2022-08-19 14:13:33 +0900

    タイトル画面を構築

---
+++ VGJ_Project_2D/Assets/Scripts/StageSelectData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="StageSelectData",menuName = "ScriptableObjects/StageSelectData")]
+public class StageSelectData : ScriptableObject
+{
+    public string StageName = "StageName";
+    public string StageSceneName = "TestScene_Okano";
+}
